90e66a466c402453c111834b82e824cee14c19a1
Srija <srijakumar419@gmail.com>
2024-12-10 05:03:53 +0000

    Updated Index.cshtml.cs

--- MyCoreApp/Pages/Index.cshtml.cs
+++ MyCoreApp/Pages/Index.cshtml.cs
@@ -5,20 +5,17 @@ namespace MyCoreApp.Pages
 {
     public class IndexModel : PageModel
     {
-        // Define public properties
         public string BirthdayPerson { get; set; }
         public string Venue { get; set; }
         public string SpecialGuest { get; set; }
         public DateTime EventDate { get; set; }
 
-        // Initialize the properties in the OnGet method
         public void OnGet()
         {
-            // Example values; adjust these as necessary
             BirthdayPerson = "Mardhal";
             Venue = "Tatipaka";
             SpecialGuest = "Rahul Dondapati";
-            EventDate = new DateTime(2024, 12, 28);  // Example event date
+            EventDate = new DateTime(2024, 12, 28);
         }
     }
 }
